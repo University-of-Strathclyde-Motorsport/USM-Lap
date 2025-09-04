c65a82872e133f8f3f974d2eef373de3ce389095
OliverRose33 <117239457+OliverRose33@users.noreply.github.com>
2025-09-04 22:26:09 +0100

    Deployed c23c780 with MkDocs version: 1.6.1

--- usmlap/__main__.py
+++
@@ -1,5 +0,0 @@
-from vehicle import brakes
-
-brakes = brakes.Brakes.from_json("library/vehicles/USM23_baseline.json")
-print(brakes)
-print(brakes.brake_lines)
--- usmlap/utils/geometry.py
+++
@@ -1,5 +0,0 @@
-from math import pi
-
-
-def area_of_circle(diameter: float) -> float:
-    return pow(diameter, 2) * (pi / 4)
