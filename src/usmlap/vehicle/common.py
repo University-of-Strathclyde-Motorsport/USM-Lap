0c7aeb1f7a5e6f098c05ab40bbc28673c576f8d9
OliverRose33 <117239457+OliverRose33@users.noreply.github.com>
2026-04-13 02:38:36 +0100

    Remove duplicate serialisation code
    Removed (de)serialisation code from Subsystem class.
    One step closer to deleting common module.

--- src/usmlap/vehicle/common.py
+++ src/usmlap/vehicle/common.py
@@ -4,8 +4,7 @@ This module contains code shared by all vehicle components.
 
 from __future__ import annotations
 
-from pathlib import Path
-from typing import Any, ClassVar, Optional, Self
+from typing import Any, ClassVar, Optional
 
 from pydantic import BaseModel
 
@@ -15,36 +14,7 @@ type JSONDict = dict[str, Any]
 class Subsystem(BaseModel):
     """
     Abstract base class for vehicle subsystems.
-
-    Provides functionality for loading from, and saving to, JSON.
     """
-
-    @classmethod
-    def from_json(cls, filepath: Path) -> Self:
-        """
-        Load a subsystem from a JSON file.
-
-        Args:
-            filepath (str): The path to the JSON file.
-
-        Returns:
-            subsystem (Self): The loaded subsystem.
-        """
-        with open(filepath, "r") as file:
-            data = file.read()
-            return cls.model_validate_json(data)
-
-    def to_json(self) -> str:
-        """
-        Converts a subsystem to a JSON string.
-
-        Args:
-            self (Subsystem): The subsystem to convert.
-
-        Returns:
-            json_string (str): The JSON string representation of the subsystem.
-        """
-        return self.model_dump_json(indent=2)
 
 
 class AbstractSubsystem(Subsystem):
